1131f0ac65f1d58ac3d893bd060d0cb11d3388be
archandanime <36922812+archandanime@users.noreply.github.com>
2021-01-14 22:07:48 +0000

    Update arch-chroot.sh

--- data/adb/service.d/arch-chroot.sh
+++ data/adb/service.d/arch-chroot.sh
@@ -4,21 +4,21 @@
 ## SCRIPT TO MOUNT CHROOT CONTAINER AND START APACHE DAEMON
 ##
 
-echo ...
+mount_container=NO
 
+chroot_apache () {
+#echo ...
 ## mount sdcard at /data/ssh/root/SDCARD before the system does
-echo ...mounting block device /dev/block/mmcblk1p1 to /data/ssh/root/SDCARD
+#echo ...mounting block device /dev/block/mmcblk1p1 to /data/ssh/root/SDCARD
 /system/bin/busybox mount /dev/block/mmcblk1p1 /data/ssh/root/SDCARD
 
 ## to mount the chroot container
-echo ...mounting chroot container /data/ssh/root/SDCARD/global/arch.img to /data/ssh/root/chrootdir
+#echo ...mounting chroot container /data/ssh/root/SDCARD/global/arch.img to /data/ssh/root/chrootdir
 /system/bin/busybox mount /data/ssh/root/SDCARD/global/arch.img /data/ssh/root/chrootdir
 
 ## start apache daemon
-echo ...starting apache daemond inside the container
+#echo ...starting apache daemond inside the container
 /system/bin/sh /data/ssh/root/start_httpd.sh
+}
 
-echo ... script finoished at $(date)
-echo 
-
-
+[[ "$mount_container" == "YES" ]] && chroot_apache
